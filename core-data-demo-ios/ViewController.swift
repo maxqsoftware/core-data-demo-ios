0040951dae0d90b065acde1c81209b9d3c2cdb04
maxqsoftware <daniel.greenheck@maxqsoftware.com>
2020-03-24 22:52:55 -0700

    Initial Commit

---
+++ core-data-demo-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  core-data-demo-ios
+//
+//  Created by Daniel Greenheck on 3/24/20.
+//  Copyright © 2020 Max Q Software LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
